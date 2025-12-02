35833a7893dbd6adc135d09a7cd9866f754f3d7b
emxnator <nazar251107@gmail.com>
2025-12-02 12:23:35 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,32 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TournamentController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/signup', function () {
+    return view('signup');
+})->name('signup');
+
+Route::get('/toernooien', [TournamentController::class, 'index'])->name('toernooien');
+
+Route::get('/contact', function () {
+    return view('contact.page');
+})->name('contact');
+
+Route::get('/teams', function () {
+    return view('teams');
+})->name('teams');
+
+Route::get('/admin', function () {
+    return view('admin');
+})->name('admin');
 
 Route::get('/dashboard', function () {
     return view('dashboard');

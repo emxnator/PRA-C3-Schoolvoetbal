c07ca9fb93c681587ba900691fd5cfc7bf5b6749
emxnator <nazar251107@gmail.com>
2025-12-02 11:05:18 +0100

    breeze installed

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TournamentController;
-use App\Http\Controllers\HomeController;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/signup', function () {
-    return view('signup');
-})->name('signup');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/toernooien', [TournamentController::class, 'index'])->name('toernooien');
-
-Route::get('/contact', function () {
-    return view('contact.page');
-})->name('contact');
-
-Route::get('/teams', function () {
-    return view('teams');
-})->name('teams');
-
-Route::get('/admin', function () {
-    return view('admin');
-})->name('admin');
-
+require __DIR__.'/auth.php';
